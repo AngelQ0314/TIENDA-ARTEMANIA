5d66f415e672b261a0c1c776162cd37f459d723a
AngelQ0314 <leonelquishpe0314@gmail.com>
2025-04-07 20:13:14 -0500

    comienzo del proyecto

---
+++ back/src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './usuarios/users.module';
+import { CategoriasModule } from './categorias/categorias.module';
+import { SubcategoriasModule } from './subcategorias/subcategorias.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [() => require('dotenv').config({ path: 'datos.env' })], 
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD') || '',
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    UsersModule,
+    CategoriasModule,
+    SubcategoriasModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
